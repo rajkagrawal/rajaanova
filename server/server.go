5869e9bc7e10d9e20929934aa92e5a46f0ee446e
raj agrawal <ragrawal@mast.com>
2018-05-28 16:05:11 +0530

    send again

--- server/server.go
+++ server/server.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 )
 
-func main(){
+func main() {
 	http.HandleFunc("/", handler)
-		err := http.ListenAndServe("127.0.0.1:8080",nil)
-	if err != nil{
-	fmt.Println(err)
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
+	if err != nil {
+		fmt.Println(err)
 	}
 
 }
 
-
-func handler(w http.ResponseWriter, r *http.Request){
-	 fmt.Fprintf(w, "hello raj")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello raj")
 }
